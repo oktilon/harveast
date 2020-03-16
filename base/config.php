0a51ec9c0e28a0c9773b0adc48f134f1a543e425
unknown <ceo.jewelsoft@gmail.com>
2020-03-16 20:07:19 +0200

    wx

--- base/config.php
+++ base/config.php
@@ -1,11 +1,17 @@
 <?php
 /* This is REAL config file */
-define('SQL_SERVER',        '');
-define('SQL_BASE',          '');
-define('SQL_USER',          '');
-define('SQL_PWD',           '');
+define('SQL_SERVER',        'mysql315.1gb.ua');
+define('SQL_BASE',          'gbua_codebase');
+define('SQL_USER',          'gbua_codebase');
+define('SQL_PWD',           '62f3e75f567');
  
-define('USER_HASH_FMT',     '');
+define('USER_HASH_FMT',     'u-#%sf!%s3vT');
 
-define('WORK_FOLDER',     	'');
+
+define('PG_SERVER',  		'5.230.195.208');
+define('PG_BASE',    		'postgres');
+define('PG_USER',    		'postgres');
+define('PG_PWD',     		'in7)(%s=qOt');
+
+define('WORK_FOLDER',     	'.');
 define('TABLE_RIGHTS',     	'spr_users_rights');
