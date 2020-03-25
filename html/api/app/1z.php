c7a9275d1e4700402b592b74d32192de385e915c
unknown <ceo.jewelsoft@gmail.com>
2020-03-25 10:19:20 +0200

    wx

---
+++ html/api/app/1z.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../../sess.php';
+session_start();
+
+if(!isset($_POST['obj'])){
+	echo json_encode(["status"=>"error"]);
+	die;
+}
+
+$obj = json_decode($_POST['obj']);
+JSON::parse($_POST['obj']);
