3a7cb48018e99875399fc1d69bfae52fa6735a95
unknown <ceo.jewelsoft@gmail.com>
2020-03-25 10:50:56 +0200

    wx

--- html/1z/index.php
+++ html/1z/index.php
@@ -3,6 +3,10 @@
 	$obj = json_encode($_REQUEST);
 	$DB->prepare("INSERT INTO st_buffer_1c SET obj=:obj");
 	$DB->bind("obj",$obj);
-	$DB->execute();
+	$q=$DB->execute();
 
-	echo json_encode($_REQUEST);
+	if($q){
+		echo json_encode(["status"=>"ok"]);
+	}else{
+		echo json_encode(["status"=>"error"]);
+	}
