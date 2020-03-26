a7665c77b89fb30bbd232522c5196b9243909f2b
unknown <ceo.jewelsoft@gmail.com>
2020-03-26 14:51:46 +0200

    wx

--- html/1z/index.php
+++ html/1z/index.php
@@ -1,20 +1,13 @@
 <?php
 	require_once '../sess.php';
-	
+
 	if(!isset($_POST['obj'])){
 		echo json_encode(["status"=>"error","err"=>"obj undefined"]);
 		die;
 	}
-	if(!isset($_POST['name'])){
-		echo json_encode(["status"=>"error","err"=>"name undefined"]);
-		die;
-	}
-	
-	$txt = json_encode($_POST);
 
-	$DB->prepare("INSERT INTO st_buffer_1c SET obj=:obj, name=:name;");
+	$DB->prepare("INSERT INTO st_buffer_1c SET obj=:obj;");
 	$DB->bind("obj", $_POST['obj']);
-	$DB->bind("name",$_POST['name']);
 	$q=$DB->execute();
 
 	if($q){
