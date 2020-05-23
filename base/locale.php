e439f425e41f65530bfda60393f3813eb1619297
Denis <oktilon@gmail.com>
2020-05-23 19:42:55 +0300

    i18n upd

--- base/locale.php
+++ base/locale.php
@@ -1,61 +1,65 @@
 <?php
     $locale = [
-        'access_denied'         => _('Access denied'),
-        'area'                  => _('Area'),
+        'access_denied'             => _('Access denied'),
+        'area'                      => _('Area'),
 
-        'begin'                 => _("Begin"),
+        'begin'                     => _("Begin"),
 
-        'clean'                 => _('Clean'),
-        'closed_already'        => _('closed already'),
-        'create_order'          => _('Create order'),
+        'clean'                     => _('Clean'),
+        'clean_needs_selection'     => _("It's need to select orders to clean"),
+        'closed_already'            => _('closed already'),
+        'create_order'              => _('Create order'),
 
-        'deleted'               => _('Deleted'),
-        'delete_islands_prmt'   => _("Delete islands less than :"),
-        'double_track'          => _('Double track'),
+        'deleted'                   => _('Deleted'),
+        'delete_islands_prmt'       => _("Delete islands less than :"),
+        'double_track'              => _('Double track'),
 
-        'end'                   => _("End"),
-        'enterprise'            => _("Enterprise"),
-        'export'                => _('Export'),
+        'end'                       => _("End"),
+        'enterprise'                => _("Enterprise"),
+        'export'                    => _('Export'),
+        'export_needs_selection'    => _("It's need to select orders to export"),
 
-        'field'                 => _("Field"),
+        'field'                     => _("Field"),
 
-        'ha'                    => _("ha"),
-        'hired'                 => _('Hired'),
+        'ha'                        => _("ha"),
+        'hired'                     => _('Hired'),
 
-        'incomplete_order_data' => _('Incomplete order data'),
-        'inters_w_orders'       => _("Intersection with orders:"),
-        'invalid_geometry'      => _('Invalid geometry'),
+        'incomplete_order_data'     => _('Incomplete order data'),
+        'inters_w_orders'           => _("Intersection with orders:"),
+        'invalid_geometry'          => _('Invalid geometry'),
 
-        'joint_area'            => _('Joint area'),
+        'joint_area'                => _('Joint area'),
 
-        'next_year'             => _('Next year'),
-        'no_fieldwork'          => _('No fieldwork'),
-        'no_gps'                => _('No Gps'),
-        'no_messages'           => _('No messages'),
-        'no_orders_found'       => _('No orders found'),
-        'no_valid_operation'    => _('No valid operation'),
-        'no_work_found'         => _('No work found'),
-        'note'                  => _("Note"),
+        'next_year'                 => _('Next year'),
+        'no_fieldwork'              => _('No fieldwork'),
+        'no_gps'                    => _('No Gps'),
+        'no_messages'               => _('No messages'),
+        'no_orders_found'           => _('No orders found'),
+        'no_valid_operation'        => _('No valid operation'),
+        'no_work_found'             => _('No work found'),
+        'note'                      => _("Note"),
 
-        'operation_width_unk'   => _('Operation width is unknown'),
-        'order'                 => _("Order"),
+        'operation_width_unk'       => _('Operation width is unknown'),
+        'order'                     => _("Order"),
 
-        'parsed'                => _('Parsed'),
-        'points_finished'       => _('Points finished'),
+        'parsed'                    => _('Parsed'),
+        'points_finished'           => _('Points finished'),
 
-        'reg_plate'             => _("Reg.plate"),
-        'recalculation'         => _('Recalculation'),
+        'reg_plate'                 => _("Reg.plate"),
+        'recalculation'             => _('Recalculation'),
 
-        'tech_operation'        => _("Tech.operation"),
-        'track'                 => _('Track'),
-        'trailer'               => _("Trailer"),
+        'server_isnt_available'     => _('Server is not available'),
 
-        'vehicle'               => _("Vehicle"),
+        'tech_operation'            => _("Tech.operation"),
+        'track'                     => _('Track'),
+        'trailer'                   => _("Trailer"),
 
-        'wrong_date_interval'   => _('Wrong date interval'),
-        'wrong_joint_id'        => _('wrong joint id'),
-        'wrong_params'          => _('Wrong params'),
-        'wrong_params_rel_log'  => _('Wrong params. Relocation log'),
-        'wrong_order'           => _('Wrong order'),
+        'vehicle'                   => _("Vehicle"),
+
+        'wrong_date_interval'       => _('Wrong date interval'),
+        'wrong_joint_id'            => _('wrong joint id'),
+        'wrong_params'              => _('Wrong params'),
+        'wrong_params_rel_log'      => _('Wrong params. Relocation log'),
+        'wrong_order'               => _('Wrong order'),
     ];
 
