d23a6a6a914c7c366b6b61313506662acd425308
unknown <ceo.jewelsoft@gmail.com>
2020-06-12 09:28:30 +0300

    wx

--- html/api/app/getFunction.php
+++ html/api/app/getFunction.php
@@ -51,10 +51,9 @@
 
 	if($user_id>0){
 		if($q[0]['dm_roles']!=''){
-			$r = $DB->select("SELECT * FROM {TABLE_RIGHTS} WHERE user_id = {$user_id} AND right_id IN ({$q[0]['dm_roles']})");
+			$r = $DB->select("SELECT * FROM spr_users_rights WHERE user_id = {$user_id} AND right_id IN ({$q[0]['dm_roles']})");
 			if(!$r){
-				$sql = "SELECT * FROM {TABLE_RIGHTS} WHERE user_id = {$user_id} AND right_id IN ({$q[0]['dm_roles']})";
-				echo json_encode(array("status"=>"error","msg"=>"Denied rights of access","sql"=>$sql));
+				echo json_encode(array("status"=>"error","msg"=>"Denied rights of access"));
 				die;
 			}
 		}
