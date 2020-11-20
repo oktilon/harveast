c77d0b82985579e7cc044f136cf744018ae2300b
Denis Stovbun <oktilon@gmail.com>
2020-11-20 15:20:27 +0200

    upd

--- base/clean_joints.php
+++ base/clean_joints.php
@@ -4,10 +4,10 @@ InfoPrefix(__FILE__);
 
 $t = time();
 Info('Run query');
-$q = $DB->prepare("SELECT count(*) from gps_order_log gol
+$q = $DB->prepare("UPDATE gps_order_log gol
                 left join gps_joint_items gji on log_id = gol.id
                 left join gps_joint gj on gj.id = gji.jnt_id
-                -- set gol.flags = gol.flags & ~8
+                set gol.flags = gol.flags & ~8
                 where gol.flags & 8 and (gj.flags & 2 OR gj.id IS NULL)")
     ->execute();
 $dt = time() - $t;
